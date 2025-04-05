57e1ee6279869b6c0916fcd82bfbddf8d7f6e4b1
vedang29 <vedang294@gmail.com>
2025-04-05 11:15:20 +0530

    Removed unused imports

--- src/components/custom/Jobs/JobListings.tsx
+++ src/components/custom/Jobs/JobListings.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Sidenav from './Sidenav'
 import Jobcard from '../utils/Jobcard'
 
--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,5 +1,4 @@
 import JobListings from "@/components/custom/Jobs/JobListings"
-import Sidenav from "@/components/custom/Jobs/Sidenav"
 import Footer from "@/components/custom/utils/Footer"
 // import Jobcard from "@/components/custom/utils/Jobcard"
 import Navbar from "@/components/custom/utils/Navbar"
